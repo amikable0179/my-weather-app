dba637b4880c00a9a44eebb30748426ed0264641
Amarachukwu Ogbu <84595051+amikable0179@users.noreply.github.com>
2022-12-28 12:39:42 -0800

    finished the scripts for search engine and current city button

--- src/index.js
+++ src/index.js
@@ -29,36 +29,47 @@ let minute = date.getMinutes();
 let weatherDate = document.querySelector(".date");
 weatherDate.innerHTML = `${dayOfWeek}, ${daysDate} <sup>th</sup> of ${month}, ${year} ${hour}:${minute}`;
 
-function temperatureValue(response) {
-  let tempValue = document.querySelector("#tempValue");
-  tempValue.innerHTML = response.data.main.temp;
+function displayWeatherCondition(response) {
+  document.querySelector("#cityName").innerHTML = response.data.name;
+  document.querySelector("#tempValue").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#weather-description").innerHTML =
+    response.data.weather[0].main;
 }
 
-function showSearch(event) {
-  event.preventDefault();
-  let citySearch = document.querySelector("#search-input").value;
+function showSearchLocation(position) {
   let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${citySearch}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(temperatureValue);
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayWeatherCondition);
 }
+
+function search(city) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function showCurrentCityDisplay(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(showSearchLocation);
+}
+
+function displaySearch(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-input").value;
+  search(city);
+}
+
+let currentCity = document.querySelector("#currentCity");
+currentCity.addEventListener("click", showCurrentCityDisplay);
+
 let mySearch = document.querySelector("#search");
-mySearch.addEventListener("submit", showSearch);
+mySearch.addEventListener("submit", displaySearch);
 
-function showTemperature(response) {
-  let myCurrentTemperature = response.data.main.temp;
-  let tempValue = document.querySelector("span#tempValue");
-  tempValue.innerHTML = Math.round(myCurrentTemperature);
-}
-
-function myLocation(position) {
-  let apiKey = "7784a4cd4aa2e0c25ead7bd96d585b8a";
-  console.log(position);
-  let city = document.querySelector("h1#cityName");
-  let lon = position.coords.longitude;
-  let lat = position.coords.latitude;
-  city.innerHTML = "Enugu, Nigeria";
-  let url = `https://api.openweathermap.org/data/2.5/weather?&appid=${apiKey}&&units=metric&lat=${lat}&lon=${lon}`;
-  axios.get(url).then(showTemperature);
-}
-
-navigator.geolocation.getCurrentPosition(myLocation);
+search("Enugu");
